14d68c8ab69ffd149531f2d6610bd808c4ffc44f
motiefard <mozhdemotieifard@gmail.com>
2024-11-13 21:02:48 +0330

    define router urls (16)
    urls.py

--- src/users/serializers.py
+++ src/users/serializers.py
@@ -5,5 +5,5 @@ class UserSerializer(serializers.ModelSerializer):
 
     class Meta :
         model = User
-        #fields = ['url', 'id', 'username', 'email', 'first_name', 'last_name']
-        fields = '__all__'
+        fields = ['url', 'id', 'username', 'email', 'first_name', 'last_name']
+        # fields = '__all__'
