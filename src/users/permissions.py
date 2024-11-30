ad9aee91155938c74b4fe452719d786f3cb51744
motiefard <mozhdemotieifard@gmail.com>
2024-11-30 19:28:07 +0330

    customize Profile viewset (26)
    add permission, change it to GenericViewSet

--- src/users/permissions.py
+++ src/users/permissions.py
@@ -3,8 +3,8 @@ from rest_framework import permissions
 
 class IsUSerOwnerOrGetAndPostOnly(permissions.BasePermission):
     """
-    custom permission for UserViewSet to only allow user to edit their own profile. 
-    Otherwise Get and Post only.
+        custom permission for UserViewSet to only allow user to edit their own info, 
+        Otherwise Get and Post only.
     """
 
     def has_permission(self, request, view):
@@ -19,3 +19,25 @@ class IsUSerOwnerOrGetAndPostOnly(permissions.BasePermission):
             return request.user == obj
         
         return False
+    
+
+class IsProfileOwnerOrReadOnly(permissions.BasePermission):
+    """
+        custom permission for ProfileViewSet to only allow user to edit their own profile, 
+        Otherwise Get and Post only.
+    """
+     
+    def has_permission(self, request, view):
+        # return super().has_permission(request, view)
+        return True
+   
+    def has_object_permission(self, request, view, obj):
+        
+        if request.method in permissions.SAFE_METHODS:
+            return True    
+        
+        if not request.user.is_anonymous:
+            return request.user.profile == obj
+        
+        return False
+    
--- src/users/viewsets.py
+++ src/users/viewsets.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.models import User
-from rest_framework import viewsets
+from rest_framework import viewsets, mixins
 from .serializers import UserSerializer, ProfileSerializer
-from .permissions import IsUSerOwnerOrGetAndPostOnly
+from .permissions import IsUSerOwnerOrGetAndPostOnly, IsProfileOwnerOrReadOnly
 from .models import Profile
 
 class UserViewSet(viewsets.ModelViewSet):
@@ -10,6 +10,7 @@ class UserViewSet(viewsets.ModelViewSet):
     serializer_class = UserSerializer
 
 
-class ProfileViewSet(viewsets.ModelViewSet):
+class ProfileViewSet(viewsets.GenericViewSet, mixins.RetrieveModelMixin, mixins.UpdateModelMixin):
+    permission_classes = [IsProfileOwnerOrReadOnly, ]
     queryset = Profile.objects.all()
     serializer_class = ProfileSerializer
