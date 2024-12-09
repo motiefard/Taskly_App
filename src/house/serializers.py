894594a9c47df058ec53f2066972912ad5798730
motiefard <mozhdemotieifard@gmail.com>
2024-12-09 22:40:33 +0330

    implement house membership functionality (34)

--- src/house/serializers.py
+++ src/house/serializers.py
@@ -4,12 +4,14 @@ from .models import House
 class HouseSerializer(serializers.ModelSerializer):
 
     members_count = serializers.IntegerField(read_only=True)
+    members = serializers.HyperlinkedRelatedField(read_only=True, many=True, view_name='profile-detail')
+    manager = serializers.HyperlinkedRelatedField(read_only=True, many=False, view_name='profile-detail')
 
     class Meta :
         model = House
         fields = ['url', 'id', 'image', 'name', 'created_on', 
                   'manager', 'description', 'members_count', 
-                  'points', 'completed_tasks_count', 
+                  'members', 'points', 'completed_tasks_count', 
                   'notcompleted_tasks_count', ]
         read_only_fields = ['points', 'completed_tasks_count', 'notcompleted_tasks_count']
 
