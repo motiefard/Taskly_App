448bb0f016d22de1770dfad78e3255b99678a7f8
motiefard <mozhdemotieifard@gmail.com>
2024-12-14 21:37:33 +0330

    implement remove-member by house-manager (37)

--- src/house/viewset.py
+++ src/house/viewset.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import User
 from rest_framework import viewsets, status
 from rest_framework.decorators import action
 from rest_framework.response import Response
@@ -48,3 +49,31 @@ class HouseViewSet(viewsets.ModelViewSet):
                 return Response({'detail': 'user not a member in this house.'}, status=status.HTTP_400_BAD_REQUEST)
         except Exception as err:
             return Response(status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+        
+    
+    @action(detail=True, methods=['post'], name='remove member', permission_classes=[])
+    def remove_member(self, request, pk=None):
+        try:
+            house = self.get_object()
+            user_id = request.data.get('user_id', None)
+            if(user_id == None):
+                return Response({'user_id':'Not provided.'}, status=status.HTTP_400_BAD_REQUEST)
+            user_profile = User.objects.get(pk=user_id).profile
+            
+            house_members = house.members
+            if(user_profile in house_members.all()):
+                house_members.remove(user_profile)
+                house.save()
+                return Response(status=status.HTTP_204_NO_CONTENT)
+            else:
+                return Response({'detail':'User not a member in this house.'}, status=status.HTTP_400_BAD_REQUEST)
+
+            # if(user_profile.house == house):
+            #     user_profile.house = None
+            #     user_profile.save()
+            #     return Response(status=status.HTTP_204_NO_CONTENT)
+            # else:
+            #     return Response({'detail':'User not a member in this house.'}, status=status.HTTP_400_BAD_REQUEST)
+
+        except Exception as err:
+            return Response({'detail':'Provided user_id does not exist.'}, status=status.HTTP_400_BAD_REQUEST)
