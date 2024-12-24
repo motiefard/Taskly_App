b9668af388d2988ada45e056048363055a76f42b
motiefard <mozhdemotieifard@gmail.com>
2024-12-24 23:25:41 +0330

    create TaskList, Attachment model (42, 43)

--- src/task/admin.py
+++ src/task/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import TaskList, Task, Attachment
 
-# Register your models here.
+class TaskListAdmin(admin.ModelAdmin):
+    readonly_fields = ['id', 'created_on']
+
+class TaskAdmin(admin.ModelAdmin):
+    readonly_fields = ['id', 'created_on']
+
+class AttachmentAdmin(admin.ModelAdmin):
+    readonly_fields = ['id', 'created_on']
+
+admin.site.register(TaskList, TaskListAdmin)
+admin.site.register(Task, TaskAdmin)
+admin.site.register(Attachment, AttachmentAdmin)
