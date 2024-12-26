e451ecb294b1a96a4df5938ed33cb03055dc613c
motiefard <mozhdemotieifard@gmail.com>
2024-12-26 20:13:43 +0330

    update task.router, change queryset on TaskViewSet (49)

--- src/task/viewsets.py
+++ src/task/viewsets.py
@@ -15,11 +15,18 @@ class TaskViewSet(viewsets.ModelViewSet):
     permission_classes =[IsAllowedToEditTaskElseNone, ]
     queryset = Task.objects.all()
     serializer_class = TaskSerializer
+    
+    def get_queryset(self):
+        queryset =  super(TaskViewSet, self).get_queryset()
+        user_profile = self.request.user.profile
+        updated_queryset = queryset.filter(created_by = user_profile)
+        return updated_queryset
 
 
 class AttachmentViewSet(mixins.CreateModelMixin, mixins.RetrieveModelMixin,
                       mixins.UpdateModelMixin, mixins.DestroyModelMixin,
-                      mixins.ListModelMixin, viewsets.GenericViewSet):
+                      #mixins.ListModelMixin, 
+                      viewsets.GenericViewSet):
     permission_class = [IsAllowedToEditAttachmentElseNone, ]
     queryset = Attachment.objects.all()
     serializer_class = AttachmentSerializer
